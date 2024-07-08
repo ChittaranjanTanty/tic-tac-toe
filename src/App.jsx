d86473911ca209f8d276d8f6dc4e77e79290be51
ChittaranjanTanty <erchitta123@gmail.com>
2024-07-08 22:30:28 +0530

    version_0.0.1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './components/card/card'
+import Grid from './components/Grid/Grid'
+function App() {
+  
+
+  return (
+    <>
+    <Grid numberOfCards={9}/>
+    
+    </>
+  )
+}
+
+export default App
